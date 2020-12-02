de9099a2edc2af482f07c1d8f42a0d4dc9d721ed
Sylvain Desodt <sylvain.desodt@faurecia.com>
2020-12-02 09:11:31 +0100

    Day 2 - part 2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Advent Of Code 2020
 
 https://adventofcode.com/2020/day/1 : 2/2
+https://adventofcode.com/2020/day/2 : 2/2
--- day2.py
+++ day2.py
@@ -2,23 +2,44 @@ import re
 
 LINE_RE = re.compile(r"(?P<mini>\d+)-(?P<maxi>\d+) (?P<letter>.): (?P<pwd>.*)")
 
-def line_is_valid_pwd(line):
+def get_info_from_line(line):
     m = LINE_RE.fullmatch(line)
     if m is None:
         raise ValueError("Line did not match regular expression", line)
     mini, maxi, letter, pwd = m .groups()
     mini, maxi = int(mini), int(maxi)
+    return (mini, maxi, letter, pwd)
+
+
+def line_is_valid_pwd_part_1(line):
+    mini, maxi, letter, pwd = get_info_from_line(line)
     count = pwd.count(letter)
     return mini <= count <= maxi
 
-def get_number_of_valid_pwd(file_path='day2_input.txt'):
+def get_number_of_valid_pwd_part1(file_path='day2_input.txt'):
     with open(file_path) as f:
-        return sum(line_is_valid_pwd(l.strip()) for l in f)
+        return sum(line_is_valid_pwd_part_1(l.strip()) for l in f)
+
+
+def line_is_valid_pwd_part_2(line):
+    mini, maxi, letter, pwd = get_info_from_line(line)
+    return sum(pwd[pos-1] == letter for pos in (mini, maxi)) == 1
+
+def get_number_of_valid_pwd_part2(file_path='day2_input.txt'):
+    with open(file_path) as f:
+        return sum(line_is_valid_pwd_part_2(l.strip()) for l in f)
+
 
 # Examples provided
-assert line_is_valid_pwd("1-3 a: abcde")
-assert not line_is_valid_pwd("1-3 b: cdefg")
-assert line_is_valid_pwd("2-9 c: ccccccccc")
+assert line_is_valid_pwd_part_1("1-3 a: abcde")
+assert not line_is_valid_pwd_part_1("1-3 b: cdefg")
+assert line_is_valid_pwd_part_1("2-9 c: ccccccccc")
+
+assert line_is_valid_pwd_part_2("1-3 a: abcde")
+assert not line_is_valid_pwd_part_2("1-3 b: cdefg")
+assert not line_is_valid_pwd_part_2("2-9 c: ccccccccc")
+
 
 # Real problem
-print(get_number_of_valid_pwd() == 542)
+print(get_number_of_valid_pwd_part1() == 542)
+print(get_number_of_valid_pwd_part2() == 360)
