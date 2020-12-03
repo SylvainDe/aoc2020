f154cd1e962544809f0d97dc9167f98ce3ba5534
SylvainDe <sylvain.desodt+github@gmail.com>
2020-12-03 09:19:34 +0100

    Day 3 - part 2

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Advent Of Code 2020
 
 https://adventofcode.com/2020/day/1 : 2/2
+
 https://adventofcode.com/2020/day/2 : 2/2
+
+https://adventofcode.com/2020/day/3 : 2/2
--- day3.py
+++ day3.py
@@ -2,14 +2,20 @@ def get_area(file_path='day3_input.txt'):
     with open(file_path) as f:
         return [l.strip() for l in f]
 
-def get_number_of_trees(area):
+def get_number_of_trees(area, dx=3, dy=1):
     x = 0
-    dx = 3
     path = []
-    for line in area:
-        path.append(line[x])
-        x = (x + dx) % len(line)
+    for i, line in enumerate(area):
+        if i % dy == 0:
+            path.append(line[x])
+            x = (x + dx) % len(line)
     return sum(x == '#' for x in path)
+
+def get_number_of_tree_for_slopes(area, slopes):
+    prod = 1
+    for dx, dy in slopes:
+        prod *= get_number_of_trees(area, dx, dy)
+    return prod
 
 # Examples provided
 area = [
@@ -26,7 +32,21 @@ area = [
     ".#..#...#.#",
 ]
 assert get_number_of_trees(area) == 7
+assert get_number_of_trees(area, 1, 1) == 2
+assert get_number_of_trees(area, 3, 1) == 7
+assert get_number_of_trees(area, 5, 1) == 3
+assert get_number_of_trees(area, 7, 1) == 4
+assert get_number_of_trees(area, 1, 2) == 2
+slopes = [
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2),
+]
+assert get_number_of_tree_for_slopes(area, slopes) == 336
 
 # Real problem
 area = get_area()
 print(get_number_of_trees(area) == 178)
+print(get_number_of_tree_for_slopes(area, slopes) == 3492520200)
