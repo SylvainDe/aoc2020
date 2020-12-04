27ca84527f1d8f0460e71be6ab62a767e2390086
SylvainDe <sylvain.desodt+github@gmail.com>
2020-12-04 22:56:54 +0100

    Add script to run them all

---
+++ all_days.py
@@ -0,0 +1,20 @@
+import day1, day2, day3, day4
+
+days = [day1, day2, day3, day4]
+
+
+def run_tests():
+    for day in days:
+        print("-", day.__name__)
+        day.run_tests()
+
+
+def get_solutions():
+    for day in days:
+        print("-", day.__name__)
+        day.get_solutions()
+
+
+if __name__ == "__main__":
+    run_tests()
+    get_solutions()
