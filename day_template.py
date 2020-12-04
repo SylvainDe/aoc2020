18679cfbfc2de2538500731c7641867aa95c2714
SylvainDe <sylvain.desodt+github@gmail.com>
2020-12-04 22:59:07 +0100

    Add template for future days

---
+++ day_template.py
@@ -0,0 +1,16 @@
+def get_xxx_from_file(file_path="dayN_input.txt"):
+    with open(file_path) as f:
+        return [l.strip() for l in f]
+
+
+def run_tests():
+    pass
+
+
+def get_solutions():
+    xxx = get_xxx_from_file()
+
+
+if __name__ == "__main__":
+    run_tests()
+    get_solutions()
