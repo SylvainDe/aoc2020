76084b3cb6d277e950f072fbb0205fdb59d7a35c
SylvainDe <sylvain.desodt+github@gmail.com>
2020-12-06 15:46:45 +0100

    Add Day 6 to all_days.py

--- all_days.py
+++ all_days.py
@@ -1,6 +1,6 @@
-import day1, day2, day3, day4, day5
+import day1, day2, day3, day4, day5, day6
 
-days = [day1, day2, day3, day4, day5]
+days = [day1, day2, day3, day4, day5, day6]
 
 
 def run_tests():
