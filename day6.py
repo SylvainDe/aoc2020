e19997b61eedea60c37a3226bb13480e65520707
Sylvain Desodt <sylvain.desodt@faurecia.com>
2020-12-07 12:17:51 +0100

    Reformat Day 6 solution

--- day6.py
+++ day6.py
@@ -2,14 +2,18 @@ def get_groups_from_file(file_path="day6_input.txt"):
     with open(file_path) as f:
         return get_groups_from_string(f.read())
 
+
 def get_groups_from_string(string):
     return [s.split() for s in string.split("\n\n")]
+
 
 def get_sum_of_any_yes_answers(groups):
     return sum(len(set("".join(g))) for g in groups)
 
+
 def get_sum_of_all_yes_answers(groups):
     return sum(len(set(g[0]).intersection(*g)) for g in groups)
+
 
 def run_tests():
     example1 = """abc
