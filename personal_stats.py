bd8a9e7cdd91492e4ba13ba227cdc48f25b2f11f
Sylvain Desodt <sylvain.desodt@faurecia.com>
2020-12-11 22:03:23 +0100

    Add Day 11 to personal stats

--- personal_stats.py
+++ personal_stats.py
@@ -8,6 +8,7 @@ Day       Time   Rank  Score       Time   Rank  Score       Delta rank  Delta ti
 
 
 stats = """
+ 11   02:20:42   8833      0   02:46:37   7158      0
  10   03:09:23  16635      0   07:32:15  15346      0
   9   02:35:35  14408      0   02:46:41  13079      0
   8   03:05:11  16551      0   03:25:06  13837      0
@@ -34,4 +35,6 @@ for line in stats.split("\n"):
         time2 = datetime.datetime.strptime(d["time2"], time_format)
         rank1 = int(d["rank1"])
         rank2 = int(d["rank2"])
-        print("{}       {:05d}        {}".format(line, rank1 - rank2, time2 - time1))
+        delta_rank = rank1 - rank2
+        delta_time = time2 - time1
+        print("{}       {:5d}        {}".format(line, delta_rank, delta_time))
