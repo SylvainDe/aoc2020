7f405f1b13fd86b08c83201cfcb3ff270b0e3f94
Sylvain Desodt <sylvain.desodt@faurecia.com>
2020-12-15 17:00:56 +0100

    Rewrite solution for day 15 with generators
    Performances are comparable.

--- day15.py
+++ day15.py
@@ -1,43 +1,65 @@
+import itertools
+
+
 def get_numbers_from_file(file_path="day15_input.txt"):
     with open(file_path) as f:
         for l in f:
             return [int(s) for s in l.split(",")]
 
 
+def yield_game(numbers):
+    turns = dict()
+    for turn, n in enumerate(numbers):
+        turns.setdefault(n, []).append(turn)
+        yield n
+    for turn in itertools.count(start=turn+1):
+        lst = turns[n]
+        n = (lst[-1] - lst[-2]) if len(lst) > 1 else 0
+        turns.setdefault(n, []).append(turn)
+        yield n
+
+
+def n_th(n, it):
+    for i, v in enumerate(it):
+        if i == n:
+            return v
+
+
 def play_game(numbers, turn):
-    if turn <= len(numbers):
-        return numbers[turn - 1]
-    ages = dict()
-    for i, n in enumerate(numbers):
-        ages.setdefault(n, []).append(i)
-    n = numbers[-1]
-    for i in range(len(numbers), turn):
-        lst_ages = ages[n]
-        if len(lst_ages) == 1:
-            n = 0
-        else:
-            n = lst_ages[-1] - lst_ages[-2]
-        ages.setdefault(n, []).append(i)
-    return n
+    return n_th(turn - 1, yield_game(numbers))
 
 
 def run_tests():
     example1 = [0, 3, 6]
+    assert list(itertools.islice(yield_game(example1), 10)) == [
+        0,
+        3,
+        6,
+        0,
+        3,
+        3,
+        1,
+        0,
+        4,
+        0,
+    ]
     assert play_game(example1, 1) == 0
     assert play_game(example1, 9) == 4
     assert play_game(example1, 10) == 0
     assert play_game(example1, 2020) == 436
-
-
-#    assert play_game(example1, 30000000) == 175594 - slow
+    assert play_game([1, 3, 2], 2020) == 1
+    assert play_game([2, 1, 3], 2020) == 10
+    assert play_game([1, 2, 3], 2020) == 27
+    assert play_game([2, 3, 1], 2020) == 78
+    assert play_game([3, 2, 1], 2020) == 438
+    assert play_game([3, 1, 2], 2020) == 1836
+    # assert play_game(example1, 30000000) == 175594 # Slow
 
 
 def get_solutions():
     numbers = get_numbers_from_file()
     print(play_game(numbers, 2020) == 273)
-
-
-#    print(play_game(numbers, 30000000) == 47205) - slow
+    # print(play_game(numbers, 30000000) == 47205) # Slow
 
 
 if __name__ == "__main__":
