9f091db3f9a8f8e7872c95518b275a16a9704362
Sylvain Desodt <sylvain.desodt@faurecia.com>
2020-12-16 10:20:07 +0100

    Day 16 - fix style

--- day16.py
+++ day16.py
@@ -2,18 +2,24 @@
 import re
 
 rules_re = re.compile(r"(?P<min>\d+)-(?P<max>\d+)")
+
+
 def get_rule_from_string(string):
     name, values = string.split(": ")
     return (name, [(int(mini), int(maxi)) for mini, maxi in rules_re.findall(values)])
 
+
 def get_ticket_from_string(string):
     return [int(n) for n in string.split(",")]
 
+
 def get_info_from_string(string):
     rules, ticket, tickets = string.split("\n\n")
-    return ([get_rule_from_string(r) for r in rules.split("\n")],
-            get_ticket_from_string(ticket.split("\n")[1]),
-            [get_ticket_from_string(s) for s in tickets.split("\n")[1:] if s])
+    return (
+        [get_rule_from_string(r) for r in rules.split("\n")],
+        get_ticket_from_string(ticket.split("\n")[1]),
+        [get_ticket_from_string(s) for s in tickets.split("\n")[1:] if s],
+    )
 
 
 def get_info_from_file(file_path="day16_input.txt"):
@@ -24,20 +30,21 @@ def get_info_from_file(file_path="day16_input.txt"):
 def value_valid_for_a_field(rules, n):
     return any(mini <= n <= maxi for _, lst in rules for mini, maxi in lst)
 
+
 def validity_rate(rules, tickets):
     return sum(n for t in tickets for n in t if not value_valid_for_a_field(rules, n))
 
 
 def guess_field_position(rules, tickets):
     positions = set(range(len(tickets[0])))
-    impossible_positions = { name: set() for name, _ in rules }
+    impossible_positions = {name: set() for name, _ in rules}
     for t in tickets:
         # Only valid tickets
         if all(value_valid_for_a_field(rules, n) for n in t):
             for i, n in enumerate(t):
                 for name, lst in rules:
-                   if not any(mini <= n <= maxi for (mini, maxi) in lst):
-                       impossible_positions[name].add(i)
+                    if not any(mini <= n <= maxi for (mini, maxi) in lst):
+                        impossible_positions[name].add(i)
     return guess_positions(impossible_positions, positions)
 
 
@@ -60,7 +67,6 @@ def guess_positions(impossible_positions, positions):
     return found
 
 
-
 def run_tests():
     example1 = """class: 1-3 or 5-7
 row: 6-11 or 33-44
@@ -75,7 +81,11 @@ nearby tickets:
 55,2,20
 38,6,12"""
     rules, ticket, tickets = get_info_from_string(example1)
-    assert rules == [('class', [(1, 3), (5, 7)]), ('row', [(6, 11), (33, 44)]), ('seat', [(13, 40), (45, 50)])]
+    assert rules == [
+        ("class", [(1, 3), (5, 7)]),
+        ("row", [(6, 11), (33, 44)]),
+        ("seat", [(13, 40), (45, 50)]),
+    ]
     assert ticket == [7, 1, 14]
     assert tickets == [[7, 3, 47], [40, 4, 50], [55, 2, 20], [38, 6, 12]]
     assert validity_rate(rules, tickets)
@@ -92,7 +102,8 @@ nearby tickets:
 15,1,5
 5,14,9"""
     rules, ticket, tickets = get_info_from_string(example2)
-    assert guess_field_position(rules, tickets) == {'seat': 2, 'class': 1, 'row': 0}
+    assert guess_field_position(rules, tickets) == {"seat": 2, "class": 1, "row": 0}
+
 
 def get_solutions():
     rules, ticket, tickets = get_info_from_file()
@@ -103,6 +114,7 @@ def get_solutions():
         if field.startswith("departure"):
             mult *= ticket[idx]
     print(mult == 1515506256421)
+
 
 if __name__ == "__main__":
     run_tests()
