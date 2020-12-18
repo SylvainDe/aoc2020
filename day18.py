ac5d33c3043ea1a1a5115bb3a65cef955ae8897a
Sylvain Desodt <sylvain.desodt@faurecia.com>
2020-12-18 10:20:17 +0100

    Day 18 - part 2

--- day18.py
+++ day18.py
@@ -33,6 +33,39 @@ def eval_expr(expr):
     return val
 
 
+def eval_expr2(expr):
+    # print(expr)
+    if not expr:
+        return 0
+    for i, c in enumerate(expr):
+        lhs, rhs = expr[:i], expr[i + 1 :]
+        if c == "(":
+            # print("expr:'%s': '%s', '%s', '%s'" % (expr, lhs, c, rhs))
+            nb_parenth = 1
+            for j, c2 in enumerate(rhs):
+                if c2 == "(":
+                    nb_parenth += 1
+                elif c2 == ")":
+                    nb_parenth -= 1
+                    if nb_parenth == 0:
+                        inside_paren, rem = rhs[:j], rhs[j + 1 :]
+                        # print("rhs:'%s': '%s' '%s'" % (rhs, inside_paren, rem))
+                        return eval_expr2(lhs + str(eval_expr2(inside_paren)) + rem)
+            raise ValueError("Unmatched parenthesis in %s" % expr)
+        elif c == "*":
+            return eval_expr2(lhs) * eval_expr2(rhs)
+    for i, c in enumerate(expr):
+        lhs, rhs = expr[:i], expr[i + 1 :]
+        if c in ("+", "-"):
+            lhs = int(lhs) if lhs else 0
+            if c == "+":
+                return lhs + eval_expr2(rhs)
+            elif c == "-":
+                return lhs - eval_expr2(rhs)
+    val = int(expr)
+    return val
+
+
 def run_tests():
     # My tests
     assert eval_expr("2 + 3") == 5
@@ -50,11 +83,29 @@ def run_tests():
     assert eval_expr("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 437
     assert eval_expr("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 12240
     assert eval_expr("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 13632
+    # My tests
+    print(eval_expr2("2 + 3") == 5)
+    print(eval_expr2("2 * 3") == 6)
+    print(eval_expr2("3 - 2") == 1)
+    print(eval_expr2("+2") == 2)
+    print(eval_expr2("-3") == -3)
+    print(eval_expr2("(2)") == 2)
+    print(eval_expr2("((((2))))") == 2)
+    print(eval_expr2("1 + 2 * 3") == 9)
+
+    print(eval_expr2("1 + (2 * 3) + (4 * (5 + 6))"))  # still becomes 51.
+    print(eval_expr2("2 * 3 + (4 * 5)"))  # becomes 46.
+    print(eval_expr2("5 + (8 * 3 + 9 + 3 * 4 * 3)"))  # becomes 1445.
+    print(eval_expr2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"))  # becomes 669060.
+    print(
+        eval_expr2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
+    )  # becomes 23340.
 
 
 def get_solutions():
     expr = get_expr_from_file()
     print(sum(eval_expr(e) for e in expr) == 53660285675207)
+    print(sum(eval_expr2(e) for e in expr) == 141993988282687)
 
 
 if __name__ == "__main__":
